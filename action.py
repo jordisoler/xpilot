978390c8c920fcb5085b652011fd776d1fa841ff
Jordi Soler <jordi.solerbusquets@gmail.com>
2016-05-21 16:44:22 +0200

    Added action interface

---
+++ action.py
@@ -0,0 +1,27 @@
+"""
+    This file implements the action interface used for all actions.
+    Its basically used for making sure that all actions can be called
+    in the same way.
+
+    This is part of the XPilot bot for the final project in
+    Scientific Python for Engineers.
+"""
+
+class Action(object):
+    """ Action object representiong a possible action of the ship. """
+
+    def start(self):
+        """ Initial procedure for the action. """
+        raise NotImplementedError("Method 'start' not implemented")
+
+    def act(self):
+        """ Main action """
+        raise NotImplementedError("Method 'act' not implemented")
+
+    def preempt(self):
+        """ Procedure to be run when the action is preempted """
+        raise NotImplementedError("Method 'preempt' not implemented")
+
+    def is_done(self):
+        """ True if the action has been completed """
+        raise NotImplementedError("Method 'is_done' not implemented")
