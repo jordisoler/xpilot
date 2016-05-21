1a425c306797f4ea80a23924078fea251b4fb9b1
Jordi Soler <jordi.solerbusquets@gmail.com>
2016-05-21 12:23:41 +0200

    First commit

---
+++ Spinner.py
@@ -0,0 +1,83 @@
+#Xpilot-AI Team 2012
+#Run: python3 Spinner.py
+import libpyAI as ai
+import math
+import os
+
+TARGET = (1000, 1000)
+DATA_FILE = "last_game.csv"
+# HOST = "147.83.26.103"
+HOST = "localhost"
+
+def angle_diff(alpha, beta):
+    """ Computes the absolute minimum distance between two angles """
+
+    ang_abs = math.fabs(alpha-beta)
+    return ang_abs if ang_abs < 180. else angle_diff(ang_abs - 360, 0)
+
+
+def move(position):
+    """ Move to a certain point in the map """
+
+    t_ang = math.atan2(position[1] - ai.selfY(), position[0] - ai.selfX())
+    t_ang_deg = t_ang * 180. / 3.1415
+    # print("Angles. Desired: %f, current: %f, diff: %f" % \
+            # (t_ang_deg, ai.selfHeadingDeg(), angle_diff(t_ang_deg, ai.selfHeadingDeg())))
+    print("Position: (%u, %u). Velocity: (%u, %u)" % \
+            (ai.selfX(), ai.selfY(), ai.selfVelX(), ai.selfVelY()))
+    if angle_diff(t_ang_deg, ai.selfHeadingDeg()) > 3.0:
+        ai.turnToDeg(int(t_ang_deg))
+    else:
+        ai.thrust(1)
+
+
+def register():
+    """ Register sensor data in a CSV file """
+
+    data = {}
+    data["X"] = ai.selfX()
+    data["Y"] = ai.selfY()
+    data["VelX"] = ai.selfVelX()
+    data["VelY"] = ai.selfVelY()
+    first_time = os.stat(DATA_FILE).st_size == 0
+
+    with open(DATA_FILE, "a") as f:
+        if first_time:
+            f.write(";".join(data) + "\n")
+        f.write(";".join(str(value) for value in data.values()) + "\n")
+
+    return first_time
+
+
+def initialise():
+    """ Initial procedure """
+    print("New Game!")
+
+
+def ai_loop():
+    """ Main loop. Automatically run at each frame. """
+
+    first_time = register()
+    if first_time:
+        initialise()
+
+    #ai.shield()
+    #ai.fireShot()
+    #ai.presskey(KEY_FIRE_LASER)
+    #print("Position: (%u, %u)" % (ai.selfX(), ai.selfY()))
+    #print("Radar: (%u, %u)" % (ai.selfRadarX(), ai.selfRadarY()))
+    #print(ai.selfHeadingDeg())
+    diff = math.sqrt((ai.selfX()-TARGET[0])**2 +(ai.selfY()-TARGET[1])**2)
+    # print("diff: %f" % diff)
+    if diff > 50.0:
+        move(TARGET)
+    else:
+        ai.thrust(0)
+        print("Done!")
+    #ai.turn(1.23)
+
+
+open(DATA_FILE, "w").close()
+
+# ai.start(AI_loop,["-name","Spinner_Jordi", "-join", "147.83.26.103"])
+ai.start(ai_loop, ["-name", "Spinner_Jordi", "-join", "localhost"])
